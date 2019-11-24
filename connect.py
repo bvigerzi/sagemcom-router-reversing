b88ef07dd1bb3905b5c8e9fe4c7c2c88d8026f83
root <brayvigi@gmail.com>
2019-11-24 18:36:38 +1100

    Read data back with appropriate timeout break

--- connect.py
+++ connect.py
@@ -1,7 +1,28 @@
 import serial
 
-baudRate = 115200
 
-ser = serial.Serial('COM4')
-print(ser.name)
-ser.close()
+def openPort():
+    baudRate = 115200
+
+    ser = serial.Serial('COM4', baudRate, timeout=1)
+    return ser
+
+
+def readBuffer(ser):
+    maxConsecutiveEmptyBytes = 5
+    currConsecEmptyBytes = 0
+    bufferSize = 1024
+    while True:
+        readBytes = ser.read(bufferSize)
+        if len(readBytes) == 0:
+            currConsecEmptyBytes = currConsecEmptyBytes + 1
+        else:
+            currConsecEmptyBytes = 0
+        if currConsecEmptyBytes >= maxConsecutiveEmptyBytes:
+            break
+        print(readBytes.decode('utf-8'))
+
+
+serialConnection = openPort()
+readBuffer(serialConnection)
+print('Finished reading the buffer... ready to write')
